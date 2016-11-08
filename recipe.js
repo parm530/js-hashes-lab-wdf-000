84daed9448b49747f8a31180d1dd3aa9267c373f
parm530 <parmsahadeo530@gmail.com>
2016-11-08 09:31:33 -0500

    Works in browser, not by running learn- due to different node version

--- recipe.js
+++ recipe.js
@@ -1,1 +1,23 @@
 'use strict';
+
+function addIngredient(recipe, ingredient_name, amount){
+  recipe[ingredient_name] = amount;
+  return recipe;
+}
+
+function removeIngredient(recipe, ingredient_name){
+  delete recipe[ingredient_name];
+  return recipe;
+}
+
+
+function updateIngredient(recipe, ingredient_name, amount){
+  recipe[ingredient_name] = amount;
+  return recipe;
+}
+
+function readRecipe(recipe){
+  for(var key in recipe){
+    console.log(`this recipe calls for ${recipe[key]} of ${key}`)
+  }
+}
